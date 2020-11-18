7b11ddde9688f9c6128801d398030d05fbefc911
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-18 18:23:52 +0100

    Get standard structure of the filter feature

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse
 from .models import *
 from django.forms import inlineformset_factory
 
+from .filters import OrderFilter
+
 def home(request):
 	orders = Order.objects.all()
 	customers = Customer.objects.all()
@@ -32,7 +34,10 @@ def customer(request, pk_test):
     orders = customer.order_set.all()
     order_count = orders.count()
 
-    context = {'customer':customer, 'orders':orders, 'order_count':order_count}
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    # orders = myFilter.qs
+
+    context = {'customer':customer, 'orders':orders, 'order_count':order_count, 'myFilter':myFilter}
     return render(request, 'accounts/customer.html',context)
 
 
