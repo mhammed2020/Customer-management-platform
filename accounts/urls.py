3041e87441069bac05512e4837c9198fac217d11
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-18 10:22:08 +0100

    Update django forms

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
 
 
 
-           path('create_order/', views.createOrder, name="create_order"),
+    path('create_order/', views.createOrder, name="create_order"),
     path('update_order/<str:pk>/', views.updateOrder, name="update_order"),
     path('delete_order/<str:pk>/', views.deleteOrder, name="delete_order"),
 ]
