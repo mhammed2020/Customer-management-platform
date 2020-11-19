bece219547114e886ae062a8c51c21d8d02d2ada
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-19 12:29:16 +0100

    Fetch order for all users

--- accounts/views.py
+++ accounts/views.py
@@ -77,7 +77,21 @@ def home(request):
 
 	return render(request, 'accounts/dashboard.html', context)
 
+
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
+	orders = request.user.customer.order_set.all()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	print('ORDERS:', orders)
+
+	context = {'orders':orders, 'total_orders':total_orders,
+	'delivered':delivered,'pending':pending}
+    	
 	context = {}
 	return render(request, 'accounts/user.html', context)
 
