5e6350b6e40729cfe918299671f69a2fcb2ad9dd
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-20 10:17:13 +0100

    remigrate database

--- project/settings.py
+++ project/settings.py
@@ -144,5 +144,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'tiktok.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'pwdpwd'
+EMAIL_HOST_USER = 'kiko.0@gmail.com'
+EMAIL_HOST_PASSWORD = 'pswdfs'
