b99f7baefba043d186a54dc8f2f5d7caeebb143b
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-20 20:44:26 +0100

    Add s3 configuration

--- project/settings.py
+++ project/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
          'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'naem',
-        'USER': 'username',
-        'PASSWORD': 'pwdpwd',
-        'HOST'  : 'aws',
-        'PORT' : '***'
+        'NAME': 'demo_1',
+        'USER': 'jeddoumed',
+        'PASSWORD': 'useradmin',
+        'HOST'  : 'database-1.cspzwixtlu0z.us-east-2.rds.amazonaws.com',
+        'PORT' : '5432'
     }
 }
 
@@ -148,5 +148,34 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'thenewcasablanca2.0@gmail.com'
-EMAIL_HOST_PASSWORD = 'sttjwxplhbamvoji'
+EMAIL_HOST_USER = 'koli.hot@gmail.com'
+EMAIL_HOST_PASSWORD = 'psword'
+
+
+
+
+
+#S3 BUCKETS CONFIG
+'''
+AWS_ACCESS_KEY_ID = '*****************'
+AWS_SECRET_ACCESS_KEY = '*****************'
+AWS_STORAGE_BUCKET_NAME = '*****************'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+'''
+
+
+'''
+<?xml version="1.0" encoding="UTF-8"?>
+<CORSConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+<CORSRule>
+    <AllowedOrigin>*</AllowedOrigin>
+    <AllowedMethod>GET</AllowedMethod>
+    <AllowedMethod>POST</AllowedMethod>
+    <AllowedMethod>PUT</AllowedMethod>
+    <AllowedHeader>*</AllowedHeader>
+</CORSRule>
+</CORSConfiguration>
+'''
